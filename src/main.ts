8a7e5f9e9073d9389db1e6207d26cf6d0c9e6bb5
Thierry Templier <templth@yahoo.fr>
2017-04-24 14:52:20 +0200

    feat(ng4): migrated to angular 4

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { HTTP_PROVIDERS, Http, XHRBackend, RequestOptions } from '@angular/http';
-import { AppComponent, environment } from './app/';
-import { ErrorNotifierService } from './app/services/error.notifier';
-import { CustomHttp } from './app/services/custom.http';
-import { AppRequestOptions, WEBAPI_URL_TOKEN } from './app/services/app.request.options';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [
-  ErrorNotifierService,
-  HTTP_PROVIDERS,
-  { provide:Http,
-    useFactory: (backend: XHRBackend, defaultOptions: RequestOptions, errorNotifier: ErrorNotifierService) => {
-      return new CustomHttp(backend, defaultOptions, errorNotifier);
-    },
-    deps: [ XHRBackend, RequestOptions, ErrorNotifierService ]
-  },
-  { provide: WEBAPI_URL_TOKEN, useValue: 'https://bookapi.apispark.net/v1' },
-  { provide: RequestOptions, useClass: AppRequestOptions }
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
